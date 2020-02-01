b55b665346ef9c5a1d8ce4b376b75dab9271118f
Coder <2515946609@qq.com>
2020-02-01 22:13:28 +0800

    遵循QQ协议,封装的框架

---
+++ src/main/java/com/jcq/Robot.java
@@ -0,0 +1,389 @@
+package com.jcq;
+import com.api.Request;
+import org.junit.Test;
+import org.meowy.cqp.jcq.entity.*;
+import org.meowy.cqp.jcq.event.JcqAppAbstract;
+
+import javax.swing.*;
+import java.util.Date;
+import java.util.TimerTask;
+import java.util.Timer;
+import java.util.TimerTask;
+public class Robot extends JcqAppAbstract implements ICQVer, IMsg, IRequest {
+
+    /**
+     * 打包后将不会调用 请不要在此事件中写其他代码
+     *
+     * @return 返回应用的ApiVer、Appid
+     */
+    public String appInfo() {
+        // 应用AppID,规则见 http://d.cqp.me/Pro/开发/基础信息#appid
+        String AppID = "com.jcq.robot";// 记住编译后的文件和json也要使用appid做文件名
+        /**
+         * 本函数【禁止】处理其他任何代码，以免发生异常情况。
+         * 如需执行初始化代码请在 startup 事件中执行（Type=1001）。
+         */
+        return CQAPIVER + "," + AppID;
+    }
+
+    /**
+     * 酷Q启动 (Type=1001)<br>
+     * 本方法会在酷Q【主线程】中被调用。<br>
+     * 请在这里执行插件初始化代码。<br>
+     * 请务必尽快返回本子程序，否则会卡住其他插件以及主程序的加载。
+     *
+     * @return 请固定返回0
+     */
+    public int startup() {
+        // 获取应用数据目录(无需储存数据时，请将此行注释)
+//        appDirectory = CQ.getAppDirectory();
+        // 返回如：D:\CoolQ\data\app\org.meowy.cqp.jcq\data\app\com.example.demo\
+        // 应用的所有数据、配置【必须】存放于此目录，避免给用户带来困扰。
+        return 0;
+    }
+
+    /**
+     * 酷Q退出 (Type=1002)<br>
+     * 本方法会在酷Q【主线程】中被调用。<br>
+     * 无论本应用是否被启用，本函数都会在酷Q退出前执行一次，请在这里执行插件关闭代码。
+     *
+     * @return 请固定返回0，返回后酷Q将很快关闭，请不要再通过线程等方式执行其他代码。
+     */
+    public int exit() {
+        return 0;
+    }
+    @Test
+    public void test(){
+        System.out.println(Util.getMinute(new Date()));
+    }
+    /**
+     * 应用已被启用 (Type=1003)<br>
+     * 当应用被启用后，将收到此事件。<br>
+     * 如果酷Q载入时应用已被启用，则在 {@link #startup startup}(Type=1001,酷Q启动) 被调用后，本函数也将被调用一次。<br>
+     * 如非必要，不建议在这里加载窗口。
+     *
+     * @return 请固定返回0。
+     */
+    public int enable() {
+        enable = true;
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                int hour=Util.getHour(new Date());
+                int minute = Util.getMinute(new Date());
+                if(hour==8&&minute==0){
+                    CQ.sendGroupMsg(621293921L,Request.getMorning());
+                }else if(hour==10&&minute==0){
+                    CQ.sendGroupMsg(621293921L,  Request.itNews());
+                }else if(hour==14&&minute==0){
+                    CQ.sendGroupMsg(621293921L,Request.getnCoVInfo());
+                }else if(hour==23&&minute==0){
+                    CQ.sendGroupMsg(621293921L,Request.getNight());
+                }else{
+                    if(minute ==0)
+                        CQ.sendGroupMsg(621293921L, Request.hotComment());
+                }
+            }
+        }, 0, 1000*60);
+        return 0;
+    }
+
+
+
+    /**
+     * 应用将被停用 (Type=1004)<br>
+     * 当应用被停用前，将收到此事件。<br>
+     * 如果酷Q载入时应用已被停用，则本函数【不会】被调用。<br>
+     * 无论本应用是否被启用，酷Q关闭前本函数都【不会】被调用。
+     *
+     * @return 请固定返回0。
+     */
+    public int disable() {
+        enable = false;
+        return 0;
+    }
+    public static String randWord(){
+        String word[]={
+                "如果你不给自己设限，世界上便没有限制你发挥的篱笆。",
+                "敢行动，梦想才生动！",
+                "大智若愚，求知若渴。",
+                "Take is cheap,Show me your code。",
+                "博观而约取，厚积而薄发。",
+                "博学之，审问之，慎思之，明辨之，笃行之",
+                "不飞则已，一飞冲天；不鸣则已，一鸣惊人",
+                "穷则独善其身，富则兼济天下",
+                "Whatever is worth doing is worth doing well",
+                "Jack of all trades and master of none"};
+        java.util.Random random=new java.util.Random();
+
+        return word[random.nextInt(10)];
+    }
+
+    /**
+     * 私聊消息 (Type=21)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subType 子类型，11/来自好友 1/来自在线状态 2/来自群 3/来自讨论组
+     * @param msgId   消息ID
+     * @param fromQQ  来源QQ
+     * @param msg     消息内容
+     * @param font    字体
+     * @return 返回值*不能*直接返回文本 如果要回复消息，请调用api发送<br>
+     * 这里 返回  {@link IMsg#MSG_INTERCEPT MSG_INTERCEPT} - 截断本条消息，不再继续处理<br>
+     * 注意：应用优先级设置为"最高"(10000)时，不得使用本返回值<br>
+     * 如果不回复消息，交由之后的应用/过滤器处理，这里 返回  {@link IMsg#MSG_IGNORE MSG_IGNORE} - 忽略本条消息
+     */
+    public int privateMsg(int subType, int msgId, long fromQQ, String msg, int font) {
+        // 这里处理消息
+        CQ.sendPrivateMsg(fromQQ, "李明哲");
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 群消息 (Type=2)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subType       子类型，目前固定为1
+     * @param msgId         消息ID
+     * @param fromGroup     来源群号
+     * @param fromQQ        来源QQ号
+     * @param fromAnonymous 来源匿名者
+     * @param msg           消息内容
+     * @param font          字体
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int groupMsg(int subType, int msgId, long fromGroup, long fromQQ, String fromAnonymous, String msg,
+                        int font) {
+        // 如果消息来自匿名者
+        if (fromQQ == 80000000L && !fromAnonymous.equals("")) {
+            // 将匿名用户信息放到 anonymous 变量中
+            Anonymous anonymous = CQ.getAnonymous(fromAnonymous);
+        }
+
+        // 解析CQ码案例 如：[CQ:at,qq=100000]
+        // 解析CQ码 常用变量为 CC(CQCode) 此变量专为CQ码这种特定格式做了解析和封装
+        // CC.analysis();// 此方法将CQ码解析为可直接读取的对象
+        // 解析消息中的QQID
+        // long qqId = CC.getAt(msg);// 此方法为简便方法，获取第一个CQ:at里的QQ号，错误时为：-1000
+        // List<Long> qqIds = CC.getAts(msg); // 此方法为获取消息中所有的CQ码对象，错误时返回 已解析的数据
+        // 解析消息中的图片
+        // String image = CC.getImage(msg);// 此方法为简便方法，获取第一个CQ:image里的图片数据，错误时打印异常到控制台，返回 null
+        // String file = CQ.getImage(image);// 获取酷Q 下载的图片地址
+
+        // 这里处理消息
+        if (fromGroup == 621293921L) {
+
+//            if(msg.contains(CC.at(CQ.getLoginQQ()))){
+//                if(msg.contains("疫情最新情况")){
+//                    String real=Util.getNews();
+//                    CQ.sendGroupMsg(fromGroup,  CC.at(fromQQ)+real );
+//                }else{
+//                    String aiCode=Util.Aicode(msg);
+//                    CQ.sendGroupMsg(fromGroup,CC.at(fromQQ)+aiCode.replace(CC.at(CQ.getLoginQQ()),""));
+//                }
+//            }
+
+            //String message=Util.getNews();
+            //  CQ.sendGroupMsg(fromGroup, CC.at(fromQQ) + """ );
+        }
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 讨论组消息 (Type=4)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype     子类型，目前固定为1
+     * @param msgId       消息ID
+     * @param fromDiscuss 来源讨论组
+     * @param fromQQ      来源QQ号
+     * @param msg         消息内容
+     * @param font        字体
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int discussMsg(int subtype, int msgId, long fromDiscuss, long fromQQ, String msg, int font) {
+        // 这里处理消息
+
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 群文件上传事件 (Type=11)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subType   子类型，目前固定为1
+     * @param sendTime  发送时间(时间戳)// 10位时间戳
+     * @param fromGroup 来源群号
+     * @param fromQQ    来源QQ号
+     * @param file      上传文件信息
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int groupUpload(int subType, int sendTime, long fromGroup, long fromQQ, String file) {
+        GroupFile groupFile = CQ.getGroupFile(file);
+        if (groupFile == null) { // 解析群文件信息，如果失败直接忽略该消息
+            return MSG_IGNORE;
+        }
+        // 这里处理消息
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 群事件-管理员变动 (Type=101)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype        子类型，1/被取消管理员 2/被设置管理员
+     * @param sendTime       发送时间(时间戳)
+     * @param fromGroup      来源群号
+     * @param beingOperateQQ 被操作QQ
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int groupAdmin(int subtype, int sendTime, long fromGroup, long beingOperateQQ) {
+        // 这里处理消息
+
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 群事件-群成员减少 (Type=102)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype        子类型，1/群员离开 2/群员被踢
+     * @param sendTime       发送时间(时间戳)
+     * @param fromGroup      来源群号
+     * @param fromQQ         操作者QQ(仅子类型为2时存在)
+     * @param beingOperateQQ 被操作QQ
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int groupMemberDecrease(int subtype, int sendTime, long fromGroup, long fromQQ, long beingOperateQQ) {
+        // 这里处理消息
+
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 群事件-群成员增加 (Type=103)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype        子类型，1/管理员已同意 2/管理员邀请
+     * @param sendTime       发送时间(时间戳)
+     * @param fromGroup      来源群号
+     * @param fromQQ         操作者QQ(即管理员QQ)
+     * @param beingOperateQQ 被操作QQ(即加群的QQ)
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int groupMemberIncrease(int subtype, int sendTime, long fromGroup, long fromQQ, long beingOperateQQ) {
+        // 这里处理消息
+        CQ.logInfo("fromGroup", "" + fromGroup);
+        CQ.logInfo("fromQQ", "" + fromQQ);
+        CQ.logInfo("beingOperateQQ", "" + beingOperateQQ);
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 群事件-群禁言 (Type=104)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subType        子类型，1/被解禁 2/被禁言
+     * @param sendTime       发送时间(时间戳)
+     * @param fromGroup      来源群号
+     * @param fromQQ         操作者QQ
+     * @param beingOperateQQ 被操作QQ(若为全群禁言/解禁，则本参数为 0)
+     * @param duration       禁言时长(单位 秒，仅子类型为2时可用)
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int groupBan(int subType, int sendTime, long fromGroup, long fromQQ, long beingOperateQQ, long duration) {
+        // 这里处理消息
+
+        return 0;
+    }
+
+    /**
+     * 好友事件-好友已添加 (Type=201)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype  子类型，目前固定为1
+     * @param sendTime 发送时间(时间戳)
+     * @param fromQQ   来源QQ
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int friendAdd(int subtype, int sendTime, long fromQQ) {
+        // 这里处理消息
+
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 请求-好友添加 (Type=301)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype      子类型，目前固定为1
+     * @param sendTime     发送时间(时间戳)
+     * @param fromQQ       来源QQ
+     * @param msg          附言
+     * @param responseFlag 反馈标识(处理请求用)
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int requestAddFriend(int subtype, int sendTime, long fromQQ, String msg, String responseFlag) {
+        // 这里处理消息
+
+        /**
+         * REQUEST_ADOPT 通过
+         * REQUEST_REFUSE 拒绝
+         */
+
+        // CQ.setFriendAddRequest(responseFlag, REQUEST_ADOPT, null); // 同意好友添加请求
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 请求-群添加 (Type=302)<br>
+     * 本方法会在酷Q【线程】中被调用。<br>
+     *
+     * @param subtype      子类型，1/他人申请入群 2/自己(即登录号)受邀入群
+     * @param sendTime     发送时间(时间戳)
+     * @param fromGroup    来源群号
+     * @param fromQQ       来源QQ
+     * @param msg          附言
+     * @param responseFlag 反馈标识(处理请求用)
+     * @return 关于返回值说明, 见 {@link #privateMsg 私聊消息} 的方法
+     */
+    public int requestAddGroup(int subtype, int sendTime, long fromGroup, long fromQQ, String msg,
+                               String responseFlag) {
+        // 这里处理消息
+
+        /**
+         * REQUEST_ADOPT 通过
+         * REQUEST_REFUSE 拒绝
+         * REQUEST_GROUP_ADD 群添加
+         * REQUEST_GROUP_INVITE 群邀请
+         */
+		/*if(subtype == 1){ // 本号为群管理，判断是否为他人申请入群
+			CQ.setGroupAddRequest(responseFlag, REQUEST_GROUP_ADD, REQUEST_ADOPT, null);// 同意入群
+		}
+		if(subtype == 2){
+			CQ.setGroupAddRequest(responseFlag, REQUEST_GROUP_INVITE, REQUEST_ADOPT, null);// 同意进受邀群
+		}*/
+        return MSG_IGNORE;
+    }
+
+    /**
+     * 本函数会在JCQ【线程】中被调用。
+     *
+     * @return 固定返回0
+     */
+    public int menuA() {
+        JOptionPane.showMessageDialog(null, "这是测试菜单A，可以在这里加载窗口");
+        return 0;
+    }
+
+    /**
+     * 本函数会在酷Q【线程】中被调用。
+     *
+     * @return 固定返回0
+     */
+    public int menuB() {
+        JOptionPane.showMessageDialog(null, "这是测试菜单B，可以在这里加载窗口");
+        return 0;
+    }
+
+}
