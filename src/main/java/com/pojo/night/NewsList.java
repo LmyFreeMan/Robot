ea5f764d5e5bba1db1fd535ad4c5f14eb6570492
Coder <2515946609@qq.com>
2020-02-01 22:12:05 +0800

    晚安心语的包

---
+++ src/main/java/com/pojo/night/NewsList.java
@@ -0,0 +1,13 @@
+package com.pojo.night;
+
+public class NewsList {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/pojo/night/Night.java
@@ -0,0 +1,33 @@
+package com.pojo.night;
+
+import java.util.List;
+
+public class Night {
+    private String msg;
+    private int code;
+    private List<NewsList> newslist;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public List<NewsList> getNewslist() {
+        return newslist;
+    }
+
+    public void setNewslist(List<NewsList> newslist) {
+        this.newslist = newslist;
+    }
+}
